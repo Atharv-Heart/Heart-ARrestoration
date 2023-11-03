d8a50c0b7fcde8b3b78b849577454063dd813a0a
Ishika Jain <ishika2413@gmail.com>
2023-11-03 08:17:50 +0530

    base

--- Assets/Scripts/ScreenshotCapture.cs
+++ Assets/Scripts/ScreenshotCapture.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class ScreenshotCapture : MonoBehaviour
 {
@@ -6,22 +8,10 @@ public class ScreenshotCapture : MonoBehaviour
     public string folderPath = "Screenshots";
     public string filenamePrefix = "Screenshot";
     public string fileExtension = "png";
-
-    // Capture a screenshot when a specific key is pressed, e.g., the "P" key
-    public KeyCode captureKey = KeyCode.P;
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Check if the capture key is pressed
-        if (Input.GetKeyDown(captureKey))
-        {
-            CaptureScreenshot();
-        }
-    }
+    public float captureFraction = 2f / 3f;
 
     // Captures a screenshot and saves it to the specified folder
-    void CaptureScreenshot()
+    private void CaptureScreenshot()
     {
         // Create a directory for screenshots if it doesn't exist
         if (!System.IO.Directory.Exists(folderPath))
@@ -33,10 +23,29 @@ public class ScreenshotCapture : MonoBehaviour
         string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
         string screenshotName = $"{filenamePrefix}_{timestamp}.{fileExtension}";
 
+        // Calculate the dimensions for capturing the top portion of the screen
+        int captureWidth = Screen.width;
+        int captureHeight = (int)(Screen.height * captureFraction);
+
+        // Create a texture to capture the screenshot
+        Texture2D screenshotTexture = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false);
+
         // Capture the screenshot
+        screenshotTexture.ReadPixels(new Rect(0, 0, captureWidth, captureHeight), 0, 0);
+        screenshotTexture.Apply();
+
+        // Encode the texture as a PNG and save it
+        byte[] bytes = screenshotTexture.EncodeToPNG();
         string screenshotPath = System.IO.Path.Combine(folderPath, screenshotName);
-        ScreenCapture.CaptureScreenshot(screenshotPath);
+        System.IO.File.WriteAllBytes(screenshotPath, bytes);
 
         Debug.Log($"Screenshot saved at: {screenshotPath}");
+    }
+
+    public void ButtonPress()
+    {
+        Debug.Log("Btn pressed");
+        CaptureScreenshot();
+        Debug.Log("FunctionWalked");
     }
 }
